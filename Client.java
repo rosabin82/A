dc3f6c18c7983b86339bca7a7494e1c347d74f9a
rosabin82 <124105888+rosabin82@users.noreply.github.com>
2023-03-28 10:07:49 +0900

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,48 @@
+package hb;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+		public static void main(String[] args) {
+			Socket socket = null; // Server와 통신하기 위한 Client의 Socket
+			BufferedReader in = null; // Server로부터 데이터를 읽어들이기 위한 입력스트림
+			BufferedReader in2 = null; // 키보드로부터 읽어들이기 위한 입력스트림
+			PrintWriter out = null; // 서버로 내보내기 위한 출력 스트림
+			InetAddress ia = null;
+			
+			try {
+				ia = InetAddress.getLocalHost(); // 서버로 접속하기 위해 서버 주소 입력
+				
+				socket = new Socket(ia, 1395); //컴퓨터 한대로 통신하기에 getLocalHost()
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // BufferedReader 선언
+				in2 = new BufferedReader(new InputStreamReader(System.in)); // BufferredReader2 선언
+				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); // 이하 동문
+				//InputStream / OutputStream = 바이트 단위 입출력을 위한 최상위 입출력 스트림 클래스
+				System.out.println(socket.toString());
+			}catch(IOException e) {
+				
+			}
+			
+			try {
+				System.out.print("서버로 보낼 매세지 : ");
+				String data = in2.readLine(); // 키보드로부터 입력
+				out.println(data);
+				out.flush(); //버퍼에 잔류하는 모든 바이트를 출력
+				
+				String str2 = in.readLine(); // 서버로부터 되돌아오는 데이터를 읽어들임
+				System.out.println("서버로부터 되돌아온 메세지 : " + str2);
+				socket.close();
+			}catch(IOException e) {
+				
+			}
+	}
+
+}
---
+++ Server.java
@@ -0,0 +1,47 @@
+package hb;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Socket socket = null; //Client와 통신하기 위한 Socket
+		ServerSocket server_socket = null; //서버생성을 위한 serverSocket
+		BufferedReader in = null; //Client로부터 데이터를 읽어들이기위한 입력스트림
+		PrintWriter out = null; //Client로 데이터를 내보내기 위한 출력 스트림 
+		
+		try {
+			server_socket = new ServerSocket(1395); //특정 포트입력
+		}catch(IOException e) {
+			System.out.println("해당 포트가 열려있습니다."); //포트열려있는지 확인 이미 열려있다면 에러구문
+		}
+		
+		try {
+			System.out.println("서버가 열렸습니다.");
+			socket = server_socket.accept(); // 서버를 생성, Client는 접속 대기
+			
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 입력스트림 생성 //문자 단위 입출력을 위한 하위 스트림 클래스
+			
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); // 출력스트림 생성
+			
+			String str = null;
+			str = in.readLine(); // Client로부터 데이터 읽어옴
+			
+			System.out.println("Client로부터 온 메세지 : " + str);
+			
+			out.write(str); // print(str)와 비슷
+			out.flush(); // 버퍼링으로 인해 기록되지 않은 데이터를 출력 스트림에 모두 출력
+			socket.close(); // 출력되지 않은 스트림은 모두 출력하고 스트림을 닫는다. 
+		}catch(IOException e) {
+			
+		}
+	}
+
+}
